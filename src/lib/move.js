957b1c82947257714cb6a4846f16c626fa595773
Richard Bjørntvedt <kjellrichard@gmail.com>
2021-05-30 20:04:40 +0200

    rmdir -> rm

--- src/lib/move.js
+++ src/lib/move.js
@@ -1,6 +1,6 @@
 const archive = require('./archive');
 const {uploadFile} = require('./s3');
-const {rmdir} = require('fs/promises');
+const {rm} = require('fs/promises');
 const os = require('os');
 const {basename} = require('path')
 module.exports.move = async function move({target, verbose = false, directory, storageClass='STANDARD'}) {    
@@ -10,7 +10,7 @@ module.exports.move = async function move({target, verbose = false, directory, s
     const archiveResult = await archive({directory,target, verbose})
     const uploadResult = await uploadFile({filename:archiveResult.filename, deleteAfter:true, verbose, storageClass})
     
-    await rmdir(directory, {recursive:true})    
+    await rm(directory, {recursive:true})    
     verbose && console.log(`Removed directory ${directory}`)
     const {bytes} = archiveResult
     const elapsed = Date.now() - start;
