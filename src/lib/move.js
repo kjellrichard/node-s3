1cfecedbbf053066a5962c71db60ff0b56d46893
Richard Bjørntvedt <kjellrichard@gmail.com>
2021-06-01 11:30:32 +0200

    configurable partSizeMb @ move

--- src/lib/move.js
+++ src/lib/move.js
@@ -1,18 +1,18 @@
 const archive = require('./archive');
-const {uploadFile} = require('./s3');
-const {rm} = require('fs/promises');
+const { uploadFile } = require('./s3');
+const { rm } = require('fs/promises');
 const os = require('os');
-const {basename} = require('path')
-module.exports.move = async function move({target, verbose = false, directory, storageClass='STANDARD'}) {    
+const { basename } = require('path')
+module.exports.move = async function move({ target, verbose = false, directory, storageClass, partSizeMb }) {
     const start = Date.now();
     if (!target)
         target = `${os.tempdir || '.'}/${basename(directory)}.zip`;
-    const archiveResult = await archive({directory,target, verbose})
-    const uploadResult = await uploadFile({filename:archiveResult.filename, deleteAfter:true, verbose, storageClass})
-    
-    await rm(directory, {recursive:true})    
+    const archiveResult = await archive({ directory, target, verbose })
+    const uploadResult = await uploadFile({ filename: archiveResult.filename, deleteAfter: true, verbose, storageClass, partSizeMb })
+
+    await rm(directory, { recursive: true })
     verbose && console.log(`Removed directory ${directory}`)
-    const {bytes} = archiveResult
+    const { bytes } = archiveResult
     const elapsed = Date.now() - start;
     return {
         bytes,
@@ -20,6 +20,6 @@ module.exports.move = async function move({target, verbose = false, directory, s
         archiveElapsed: archiveResult.elapsed,
         uploadElapsed: uploadResult.elapsed,
         name: uploadResult.name,
-        storageClass       
+        storageClass
     }
 }
